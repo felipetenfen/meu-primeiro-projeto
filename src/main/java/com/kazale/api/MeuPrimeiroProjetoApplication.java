6655d173a4a9762cfdaae892391f6d7ab0e3b466
Felipe <lipetenfen@gmail.com>
2018-11-22 11:10:33 -0200

    Alteração no arquivo principal da aplicação.

--- src/main/java/com/kazale/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/kazale/api/MeuPrimeiroProjetoApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MeuPrimeiroProjetoApplication {
-
-	public static void main(String[] args) {
+public class MeuPrimeiroProjetoApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+		
+		System.out.println("Meu primeiro projeto!");
 	}
 }
